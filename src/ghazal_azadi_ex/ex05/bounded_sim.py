41815ecfd145ae6905c058157ee0f69495351ff8
Ghazal Azadi <ghaz@nmbu.no>
2019-11-02 10:27:43 +0100

    bounded simulation done

--- src/ghazal_azadi_ex/ex05/bounded_sim.py
+++ src/ghazal_azadi_ex/ex05/bounded_sim.py
@@ -1,4 +1,4 @@
-from walker_sim import Walker, Simulation
+from walker_sim import Walker
 import random
 
 __author__ = 'Ghazal_Azadi'
@@ -29,22 +29,36 @@ class BoundedWalker(Walker):
         return self.steps
 
 
-class BoundedSimulation(Simulation):
+class BoundedSimulation:
     def __init__(self, start, home, seed, left_limit, right_limit):
-        super().__init__(start, home, seed)
-
+        self.start = start
+        self.home = home
         self.left_limit = left_limit
         self.right_limit = right_limit
+        random.seed(seed)
 
     def bounded_single_walk(self):
         walker = BoundedWalker(self.start, self.home, self.left_limit,
                                self.right_limit)
-        while not walker.is_at_home():
-            walker.move()
-        return walker.get_steps()
+        return walker.move()
+
 
     def bounded_run_simulation(self, num_walks):
         list_of_walks = []
         for walks in range(num_walks):
             list_of_walks.append(self.bounded_single_walk())
         return list_of_walks
+
+
+if __name__ == "__main__":
+    for left_limit in (0, -10, -100, -1000, -10000):
+        bounded_walker = []
+        bounded_simulation = []
+        walk_1 = BoundedWalker(0, 20, left_limit, 20)
+        walk_2 = BoundedSimulation(0, 20, 12345, left_limit, 20)
+        bounded_walker.append(walk_1.move())
+        bounded_simulation.append(walk_2.bounded_run_simulation(20))
+        print(f'Left Limit: {left_limit} -> Path lengths of bounded walker is:'
+              f' {bounded_walker}')
+        print(f'Left Limit: {left_limit} -> Path lengths of bounded simulation'
+              f' for 20 walks is: {bounded_simulation}')
